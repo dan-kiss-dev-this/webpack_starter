38a9fb62e211176f82d361572f9e984386acc7b0
Daniel Kiss <dkiss@phase2technology.com>
2018-10-09 15:34:34 -0700

    setup weback.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: path.resolve(__dirname, 'src/index.js'),
@@ -13,5 +14,6 @@ module.exports = {
                 use: ['babel-loader']
             }
         ]
-    }
+    },
+    plugins: [ new HtmlWebpackPlugin() ]
 }
